b51159cac79acb39c48d3db5bb54b16a9b872af6
nikitaChihranov <nikita.chihranov@gmail.com>
2019-01-20 15:15:26 +0400

    Fix bugs.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,8 @@ export class AppComponent implements OnInit {
   recipe: Recipe;
   photosToUpload: File[];
   photosToUpdate: File[];
-  deletedRecipe: Recipe;
+  deletedRecipe: {};
+  updatedRecipe: {};
   constructor(private recipeService: RecipeService) {
   }
   ngOnInit() {
@@ -32,11 +33,13 @@ export class AppComponent implements OnInit {
   update(form) {
     this.recipe = {...this.recipe, ...form.value};
     this.recipeService.updateRecipe(this.recipe.name, this.recipe).subscribe((res) => {
-      this.recipeService.updatePhoto(this.photosToUpdate, res).subscribe(() => {});
+      this.recipeService.updatePhoto(this.photosToUpdate, res).subscribe((response) => {
+        this.updatedRecipe = response;
+      });
     });
   }
   delete(form) {
-    this.recipeService.deleteRecipe(form.value).subscribe((res) => this.deletedRecipe = res);
+    this.recipeService.deleteRecipe(form.value).subscribe((res) => this.deletedRecipe = res ? res : {});
   }
   deleteAllRecipes() {
     this.recipeService.deleteAllRecipes().subscribe(() => {});
